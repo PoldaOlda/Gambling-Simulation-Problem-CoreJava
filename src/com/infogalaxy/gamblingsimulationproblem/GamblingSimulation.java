46424d5f760f42a330c9c277792049db0c8d468e
PoldaOlda <poldaolda123@gmail.com>
2024-03-05 09:48:00 +0530

    [ADDED]:Tahir - Gambling Simulation Setup

---
+++ src/com/infogalaxy/gamblingsimulationproblem/GamblingSimulation.java
@@ -0,0 +1,11 @@
+package com.infogalaxy.gamblingsimulationproblem;
+
+public class GamblingSimulation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Gambling SImulation Problem developed by Tahir Mansuri.");
+
+	}
+
+}
