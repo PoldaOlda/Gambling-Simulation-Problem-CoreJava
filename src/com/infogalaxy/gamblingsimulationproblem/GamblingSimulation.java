d295e30a88c4030063ab38a9ba6eefe35263ff7d
PoldaOlda <poldaolda123@gmail.com>
2024-03-05 09:58:23 +0530

    [ADDED]:Tahir - Setting up Stack and Bet

--- src/com/infogalaxy/gamblingsimulationproblem/GamblingSimulation.java
+++ src/com/infogalaxy/gamblingsimulationproblem/GamblingSimulation.java
@@ -2,10 +2,17 @@ package com.infogalaxy.gamblingsimulationproblem;
 
 public class GamblingSimulation {
 
+	private static final int STACK_PER_DAY = 100;
+	private static final int BET_PER_GAME = 1;
+	
+	public static void gameStatus() {
+		System.out.println("Stack Available="+STACK_PER_DAY);
+		System.out.println("Bet Per Game="+BET_PER_GAME);
+	}
+	
 	public static void main(String[] args) {
-		
 		System.out.println("Welcome to Gambling SImulation Problem developed by Tahir Mansuri.");
-
+		gameStatus();
 	}
 
 }
